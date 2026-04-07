73e42d1d39437c0154587df4bb5031555a4263ae
Winter <you@example.com>
2026-04-08 01:04:20 +0200

    fixed niri recolouring via wallpaper changes

--- data/scripts/reload-niri.sh
+++ data/scripts/reload-niri.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 CACHE="${XDG_CACHE_HOME:-$HOME/.cache}/skwd-wall"
-COLOR=$(cat "$CACHE/niri-primary-color" 2>/dev/null | tr -d '[:space:]')
-[ -z "$COLOR" ] && exit 0
+COLORS="$CACHE/niri-primary-color"
+[ -f "$COLORS" ] || exit 0
+
+PRIMARY=$(sed -n '1p' "$COLORS" | tr -d '[:space:]')
+INACTIVE=$(sed -n '2p' "$COLORS" | tr -d '[:space:]')
+[ -z "$PRIMARY" ] && exit 0
 
 NIRI_CFG="$HOME/.config/niri/config.kdl"
 [ -f "$NIRI_CFG" ] || exit 0
 
-sed -i "s/active-color \"#[0-9a-fA-F]*\" \/\/ matugen:active-color/active-color \"#${COLOR}\" \/\/ matugen:active-color/" "$NIRI_CFG"
+sed -i "s/active-color \"#[0-9a-fA-F]*\" \/\/ matugen:active-color/active-color \"#${PRIMARY}\" \/\/ matugen:active-color/g" "$NIRI_CFG"
+
+if [ -n "$INACTIVE" ]; then
+    sed -i "s/inactive-color \"#[0-9a-fA-F]*\" \/\/ matugen:inactive-color/inactive-color \"#${INACTIVE}\" \/\/ matugen:inactive-color/g" "$NIRI_CFG"
+fi
+
+niri msg action load-config-file 2>/dev/null || true
